9d36d73597b7e30f750dbf447cfee955fb3a5fef
jeeva-tj <72784973+jeeva-tj@users.noreply.github.com>
2022-06-28 12:13:52 +0530

    updated hr

--- hr.py
+++ hr.py
@@ -672,7 +672,8 @@ def page5_st():
 #         fig1.update_yaxes(showgrid=False)
 #         fig1.update_traces(marker_color="#3EC1CD")
 #         st.plotly_chart(fig1)
-          st.dataframe(df_leave1)
+          df_leave2=df_leave1
+          st.dataframe(df_leave2)
 
 def page6_st():
 
